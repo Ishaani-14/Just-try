4582d341386ed1945333fbad99a015ecfb519ff3
Ishaani-14 <141027963+Ishaani-14@users.noreply.github.com>
2024-03-20 11:06:13 +0530

    Update merge_sort.js

--- merge_sort.js
+++ merge_sort.js
@@ -1,9 +1,4 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
+
 
 function Merge()
 {
@@ -73,7 +68,7 @@ function merge_partition(start,end)
     }
 }
 
-/*
+
     *****************
     DONE BY:-   TUMMALA KETHAN
     
